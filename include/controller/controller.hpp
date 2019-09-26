d0dc6e2f4119827fbf257cf4e4aacf6214ceb6ea
Martcus18 <marcocapotondi@fastwebnet.it>
2019-09-26 18:35:54 +0200

    Added and included LIMBO library

--- include/controller/controller.hpp
+++ include/controller/controller.hpp
@@ -66,8 +66,22 @@
 #include <chrono>
 #include <utils/data_utils.hpp>
 
+#include <limbo/kernel/exp.hpp>
+#include <limbo/kernel/squared_exp_ard.hpp>
+#include <limbo/mean/data.hpp>
+#include <limbo/model/gp.hpp>
+#include <limbo/model/sparsified_gp.hpp>
+#include <limbo/model/gp/kernel_lf_opt.hpp>
+#include <limbo/model/gp/mean_lf_opt.hpp>
+#include <limbo/tools.hpp>
+#include <limbo/tools/macros.hpp>
+#include <limbo/serialize/text_archive.hpp>
+#include <limbo/stop/max_iterations.hpp>
+
 typedef Eigen::Matrix< double , NUMBER_OF_JOINTS , 1> Kuka_Vec;
 typedef Eigen::Matrix< double , NUMBER_OF_JOINTS , NUMBER_OF_JOINTS> Kuka_Mat;
+
+using namespace limbo;
 
 class controller
 {
@@ -80,6 +94,23 @@ class controller
                 data_manager writer();
                 Kp.diagonal() << 50,50,50,50,50,50,50;
                 Kd.diagonal() << 1,1,1,1,1,1,1;
+                
+                for(int i=0;i<NUMBER_OF_JOINTS;i++)
+                {
+                    GP_t temp_gp(NUMBER_OF_JOINTS*3,1);
+                    gp_container.push_back(temp_gp);
+                }
+                Eigen::MatrixXd temp;
+                Eigen::MatrixXd temp2;
+                Eigen::MatrixXd temp3;
+                Eigen::MatrixXd temp4;
+    
+                temp = A * B;
+                temp2 = B * B.transpose();
+                temp3 = temp * temp.transpose();
+                W = temp2 + temp3;
+                temp4 = -(B.transpose() + temp.transpose());
+                Y = temp4 * W.inverse();
         };
 
         //Feedback Linearization VIRTUAL
@@ -123,9 +154,29 @@ class controller
 
         Eigen::DiagonalMatrix<double, NUMBER_OF_JOINTS> Kp;
         Eigen::DiagonalMatrix<double, NUMBER_OF_JOINTS> Kd;
-
+        
+        //GP parameters
+        struct Params 
+        {
+            struct kernel_exp 
+            {
+                BO_PARAM(double, sigma_sq, 1.0);
+                BO_PARAM(double, l, 0.2);
+            };
+            struct kernel : public defaults::kernel {};
+            struct kernel_squared_exp_ard : public defaults::kernel_squared_exp_ard {};
+            struct opt_rprop : public defaults::opt_rprop {};    
+        };
+        using Kernel_t = kernel::SquaredExpARD<Params>;
+        using Mean_t = mean::Data<Params>;
+        using GP_t = model::GP<Params, Kernel_t, Mean_t, model::gp::KernelLFOpt<Params>>;
+        std::vector<GP_t > gp_container;
     protected:
 
+        Eigen::Matrix2d A;
+        Eigen::Matrix<double, 2, 1>  B;
+        Eigen::Matrix2d W;
+        Eigen::Matrix<double, 1, 2> Y;
         double unwrap_angle(double angle_old, double angle_new);
 };
 #endif /* CONTROLLER_HPP_ */
---
+++ src/gp.cpp
@@ -0,0 +1,175 @@
+//| Copyright Inria May 2015
+//| This project has received funding from the European Research Council (ERC) under
+//| the European Union's Horizon 2020 research and innovation programme (grant
+//| agreement No 637972) - see http://www.resibots.eu
+//|
+//| Contributor(s):
+//|   - Jean-Baptiste Mouret (jean-baptiste.mouret@inria.fr)
+//|   - Antoine Cully (antoinecully@gmail.com)
+//|   - Konstantinos Chatzilygeroudis (konstantinos.chatzilygeroudis@inria.fr)
+//|   - Federico Allocati (fede.allocati@gmail.com)
+//|   - Vaios Papaspyros (b.papaspyros@gmail.com)
+//|   - Roberto Rama (bertoski@gmail.com)
+//|
+//| This software is a computer library whose purpose is to optimize continuous,
+//| black-box functions. It mainly implements Gaussian processes and Bayesian
+//| optimization.
+//| Main repository: http://github.com/resibots/limbo
+//| Documentation: http://www.resibots.eu/limbo
+//|
+//| This software is governed by the CeCILL-C license under French law and
+//| abiding by the rules of distribution of free software.  You can  use,
+//| modify and/ or redistribute the software under the terms of the CeCILL-C
+//| license as circulated by CEA, CNRS and INRIA at the following URL
+//| "http://www.cecill.info".
+//|
+//| As a counterpart to the access to the source code and  rights to copy,
+//| modify and redistribute granted by the license, users are provided only
+//| with a limited warranty  and the software's author,  the holder of the
+//| economic rights,  and the successive licensors  have only  limited
+//| liability.
+//|
+//| In this respect, the user's attention is drawn to the risks associated
+//| with loading,  using,  modifying and/or developing or reproducing the
+//| software by the user in light of its specific status of free software,
+//| that may mean  that it is complicated to manipulate,  and  that  also
+//| therefore means  that it is reserved for developers  and  experienced
+//| professionals having in-depth computer knowledge. Users are therefore
+//| encouraged to load and test the software's suitability as regards their
+//| requirements in conditions enabling the security of their systems and/or
+//| data to be ensured and,  more generally, to use and operate it in the
+//| same conditions as regards security.
+//|
+//| The fact that you are presently reading this means that you have had
+//| knowledge of the CeCILL-C license and that you accept its terms.
+//|
+#include <fstream>
+#include <iostream>
+#include <string.h>
+
+#include <limbo/kernel/exp.hpp>
+#include <limbo/kernel/squared_exp_ard.hpp>
+#include <limbo/mean/data.hpp>
+#include <limbo/model/gp.hpp>
+#include <limbo/model/sparsified_gp.hpp>
+#include <limbo/model/gp/kernel_lf_opt.hpp>
+#include <limbo/model/gp/mean_lf_opt.hpp>
+#include <limbo/tools.hpp>
+#include <limbo/tools/macros.hpp>
+
+#include <limbo/serialize/text_archive.hpp>
+#include <utils/data_utils.hpp>
+#include <limbo/stop/max_iterations.hpp>
+#include <chrono>
+
+// this tutorials shows how to use a Gaussian process for regression
+
+using namespace limbo;
+using namespace std;
+using namespace Eigen;
+using namespace boost;
+
+
+
+struct Params {
+    struct kernel_exp 
+    {
+        BO_PARAM(double, sigma_sq, 1.0);
+        BO_PARAM(double, l, 0.2);
+    };
+    struct kernel : public defaults::kernel {
+    };
+    struct kernel_squared_exp_ard : public defaults::kernel_squared_exp_ard {
+    };
+    struct opt_rprop : public defaults::opt_rprop {
+    };
+    //In case of sparse GP define the number of maximum points as parameters
+    /*
+    struct model_sparse_gp
+    {
+        BO_PARAM(int, max_points, 200);
+    };
+    */
+};
+
+using Kernel_t = kernel::SquaredExpARD<Params>;
+using Mean_t = mean::Data<Params>;
+//Using exact model 
+using GP_t = model::GP<Params, Kernel_t, Mean_t, model::gp::KernelLFOpt<Params>>;
+
+/*
+using Kernel_t = kernel::Exp<Params>;
+using Mean_t = mean::Data<Params>;
+using GP_t = model::GP<Params, Kernel_t, Mean_t>;
+*/
+
+//Using sparse model for accelerating the process
+//Optimize hyperparameters for kernel matching
+//using GP_t = model::SparsifiedGP<Params, Kernel_t, Mean_t, model::gp::KernelLFOpt<Params>>;
+//Optimize hyperparameters for mean matching
+//using GP_t = model::SparsifiedGP<Params, Kernel_t, Mean_t, model::gp::MeanLFOpt<Params>>;
+
+int main(int argc, char** argv)
+{       
+
+    std::string Xtrain = "Xtrain.txt";
+    std::string Ytrain = "Ytrain.txt";
+    std::string Prediction_file = "gp_prediction.dat";
+    data_manager dataX;
+    data_manager dataY;
+    std::vector<Eigen::VectorXd> X;
+    std::vector<Eigen::VectorXd> Y;
+    
+
+
+    GP_t gp(21, 1);
+
+    //Loading gp hyperparameters
+    gp.load<serialize::TextArchive>("myGP");
+
+    //Specify Input and Output dimension
+    dataX.read_data(Xtrain,X,21);
+    
+    dataY.read_data(Ytrain,Y,1);
+
+    dataX.normalize_data(X);
+    //dataX.de_normalize_data(X);
+
+    dataY.normalize_data(Y);
+    //dataY.de_normalize_data(Y);
+       
+
+
+
+    gp.compute(X, Y, false);
+    //gp.optimize_hyperparams();
+
+    // Sometimes is useful to save an optimized GP
+    //gp.save<serialize::TextArchive>("myGP");
+
+
+
+    
+    std::vector<Eigen::VectorXd> Prediction;
+    Eigen::VectorXd mu;
+    Eigen::VectorXd v;
+    double sigma;
+    
+    for (int i = 0; i < 500; ++i) 
+    {
+        v = X[i];
+        //chrono::steady_clock sc;   // create an object of `steady_clock` class
+        //auto start = sc.now(); 
+        std::tie(mu, sigma) = gp.query(v);
+        //auto end = sc.now();
+        //auto time_span = static_cast<chrono::duration<double>>(end - start);   // measure time span between start & end
+        //std::cout<<"Operation took: "<<time_span.count()<<" seconds !!!" << "\n";
+        Prediction.push_back(mu);
+    }
+
+
+    dataY.de_normalize_data(Prediction);
+    //Writing limbo format of file
+    dataY.write_data(Prediction_file,Prediction);
+    return 0;
+}
